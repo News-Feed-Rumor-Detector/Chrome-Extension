2f39e36cd6e48125051bdea4413896230c64dfde
Deffo0 <ahmeddeffo777@gmail.com>
2024-02-12 14:39:16 +0200

    catch the text selection event

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
-let isEnabled = false;
+var isEnabled = false;
+
+chrome.storage.sync.get(['isToggleEnabled'], function(result) {
+  isEnabled = result.isToggleEnabled || false;
+});
 
 console.log('Content script is loaded');
-console.log(document.URL);
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action == 'toggle') {
     console.log('Toggle message received');
@@ -9,48 +13,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
-document.addEventListener('click', function() {
-  console.log('Clicked on the page');
-    checkRumour();
-
+// Catch text selection using cursor
+document.addEventListener("mouseup", function() {
+  checkRumour();
 });
 
 function checkRumour() {
   var selectedText = window.getSelection().toString();
-  console.log("may I ask for: " + selectedText);
-  if (isEnabled && selectedText.length > 0) {
+  if (isEnabled && selectedText) {
     // TODO: Integrate with the NFRD Server
     const isRumour = /* Make your API call and get the boolean response */ true;
-    console.log(selectedText);
     showPopup(selectedText, isRumour);
   }
 }
 
 function showPopup(selectedText, isRumour) {
-  const popup = document.createElement('div');
-  popup.className = isRumour?'rumor-popup' : 'fact-popup';
-  popup.textContent = `${isRumour? `Rumor Alert:` : `Fact Alert` }${selectedText}`;
-
-  // Add styles to the popup
-  const popupStyles = `
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: ${isRumour? '#ff0000' : '#00ff00'}
-    color: #ffffff; /* White text color */
-    padding: 10px;
-    border-radius: 5px;
-    z-index: 1000000; /* Higher z-index to make sure it's on top */
-  `;
-
-  popup.style.cssText = popupStyles;
-
-  // Append the popup to the body
-  document.body.appendChild(popup);
-
-  // Remove the popup after a certain duration (e.g., 3 seconds)
-  setTimeout(() => {
-    document.body.removeChild(popup);
-  }, 5000);
+  const alertMessage = ` NFRD Message: "${selectedText}" is ${isRumour ? 'Rumour' : 'Fact'}`;
+  alert(alertMessage);
 }
