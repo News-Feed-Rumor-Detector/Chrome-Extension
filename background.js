5548939d956b1ccb936529829ba15bc5fb2296a7
Deffo0 <ahmeddeffo777@gmail.com>
2024-02-12 14:59:10 +0200

    remove unwanted logs

--- background.js
+++ background.js
@@ -22,8 +22,6 @@ chrome.runtime.onMessage.addListener(async function(request, sender, sendRespons
     }
 
     if (request.action == 'toggle') {
-        console.log('Toggle message received');
-
         // Relay the message to the active tab's content script
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             const activeTab = tabs[0];
