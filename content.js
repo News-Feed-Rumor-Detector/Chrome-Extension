413c20eb33e4e112503099ba71e96fa70f0afa22
Deffo0 <ahmeddeffo777@gmail.com>
2024-06-18 22:23:15 +0300

    connect to the inference API from hugging face

--- content.js
+++ content.js
@@ -15,19 +15,66 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 // Catch text selection using cursor
 document.addEventListener("mouseup", function() {
+  setTimeout('', 200);
   checkRumour();
 });
 
 function checkRumour() {
   var selectedText = window.getSelection().toString();
   if (isEnabled && selectedText) {
-    // TODO: Integrate with the NFRD Server
-    const isRumour = /* Make your API call and get the boolean response */ true;
-    showPopup(selectedText, isRumour);
+    query({"inputs": selectedText})
+    .then(response => response.json())
+    .then(data => {
+    	console.log(data);
+			parsedData = parseData(data);      
+			const isRumour = parsedData.isRumour;
+      const confidenceScore = parsedData.confidenceScore;
+      showPopup(isRumour, confidenceScore);
+    })
+    .catch(error => {
+      console.error('Error fetching predictions:', error);
+    });
   }
 }
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/NFRD/nfrd-model",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const relayedResponse = await response;
+	return relayedResponse;
+}
 
-function showPopup(selectedText, isRumour) {
-  const alertMessage = ` NFRD Message: "${selectedText}" is ${isRumour ? 'Rumour' : 'Fact'}`;
+function parseData(data){
+	// Extract the relevant information
+	const label1 = data[0][0];
+	const label0 = data[0][1];
+
+	// Determine if the prediction is a rumour or not
+	const isRumour = label1.score > label0.score;
+
+	// Get the confidence score of the decision
+	const confidenceScore = isRumour ? label1.score : label0.score;
+
+	// Create the resulting JSON
+	const result = {
+		  isRumour: isRumour,
+		  confidenceScore: confidenceScore
+	};
+	return result;
+}
+
+function showPopup(isRumour, confidenceScore) {
+  const formattedConfidenceScore = parseFloat(confidenceScore).toFixed(3);
+
+  const alertMessage = `Based on our analysis, the selected text is most likely a fact with a confidence score of ${formattedConfidenceScore}. Please consider this when sharing or making decisions.`;
+  if (isRumour){
+    const alertMessage = `Based on our analysis, the selected text is most likely a rumour with a confidence score of ${formattedConfidenceScore}. Please verify the information from reliable sources before sharing or making decisions.`;
+  }
+  
   alert(alertMessage);
 }
